99d5e0a5fdf06f19de865f1d9d504e6850c8849c
Michael van der Luit <3525053+mvdluit@users.noreply.github.com>
2022-02-23 14:25:59 +0100

    replace witch.tv to prevent false positives on twitch.tv domains

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const stopPhishing = require('stop-discord-phishing');
 const fs = require('fs');
 
+const witchtv = {
+  word: 'witch.tv',
+  replaceWith: [
+    'http://witch.tv',
+    'https://witch.tv',
+    'http://www.witch.tv',
+    'https://witch.tv',
+  ],
+};
+
 async function listPhishingDomains() {
   let domains = await stopPhishing.listDomains();
+
+  const checkedDomains = findAndReplace(domains, witchtv);
+
   let fmt = {
     private: true,
-    words: domains,
+    words: checkedDomains,
   };
   fs.writeFileSync('scam-domains.json', JSON.stringify(fmt, null, 4));
 }
@@ -13,11 +26,24 @@ async function listPhishingDomains() {
 async function listSuspiciousDomains() {
   let domains = await stopPhishing.listSuspicious();
 
+  const checkedDomains = findAndReplace(domains, witchtv);
+
   let fmt = {
     private: true,
-    words: domains,
+    words: checkedDomains,
   };
   fs.writeFileSync('suspicious-domains.json', JSON.stringify(fmt, null, 4));
+}
+
+function findAndReplace(domains, options) {
+  if (domains.includes(options.word)) {
+    let index = domains.indexOf(options.word);
+    domains.splice(index, 1);
+    domains.push(...options.replaceWith);
+    return domains.sort();
+  } else {
+    return domains;
+  }
 }
 
 listPhishingDomains();
